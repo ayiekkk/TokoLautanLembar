8cadf71d7a310a64fccd1f23c3ae3b795db03a81
Mohamad Alief Firmansyah <mohamadalief547@gmail.com>
2026-05-22 06:52:55 +0700

    fix : dashboard

--- routes/admin.php
+++ routes/admin.php
@@ -1,11 +1,30 @@
 <?php
 
-$page = isset($_GET['page']) ? $_GET['page'] : "dashboard";
+$page = $_GET['page'] ?? 'dashboard';
 
-if ($page == "dashboard") {
-    include __DIR__ . "/../pages/admin/dashboard/dashboard.php";
-} elseif ($page == "tambah-buku") {
-    include __DIR__ . "/../pages/admin/buku/form_buku.php";
-} elseif ($page == "tambah-kategori") {
-    include __DIR__ . "/../pages/admin/kategori/form_kategori.php";
+switch ($page) {
+
+    case 'dashboard':
+
+        include __DIR__ . '/../pages/admin/dashboard/dashboard.php';
+
+        break;
+
+    case 'tambah-buku':
+
+        include __DIR__ . '/../pages/admin/buku/form_buku.php';
+
+        break;
+
+    case 'tambah-kategori':
+
+        include __DIR__ . '/../pages/admin/kategori/form_kategori.php';
+
+        break;
+
+    default:
+
+        echo "<h1>404 - Halaman Tidak Ditemukan</h1>";
+
+        break;
 }
